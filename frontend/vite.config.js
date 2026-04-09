b546df6cbf91012888ac1182a47cc07e48d017e7
Bavanetha M R <bavanethamr@gmail.com>
2026-04-09 13:23:25 +0530

    backend url added

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5001',
+        target: 'https://smartedu-t2x0.onrender.com',
         changeOrigin: true,
       },
     },
